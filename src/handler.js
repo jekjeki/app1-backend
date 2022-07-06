33fd55a865e9c16187efad57cd7745581d651a34
jekjeki <90241176+jekjeki@users.noreply.github.com>
2022-07-06 12:13:34 +0700

    Add files via upload

---
+++ src/handler.js
@@ -0,0 +1,136 @@
+import nanoid from 'nanoid'
+const notes = require('./notes')
+
+const addNoteHandler = (request, h) => {
+    const {tittle, tags, body} = request.payload
+    const id = nanoid(16)
+    const createdAt = new Date().toISOString()
+    const updatedAt = createdAt
+
+    const newNote = {
+        tittle, tags, body, id, createdAt, updatedAt
+    }
+
+    notes.push(newNote)
+
+    const isSuccess = notes.filter((note)=> note.id === id).length > 0
+
+    if(isSuccess){
+       const response = h.response({
+        status: "success",
+        messsage: "catatan berhasil ditambahkan",
+        data: {
+            noteId : id
+        }
+       })
+       response.code(201)
+       return response 
+    }
+
+    const response = h.response({
+        status: "fail",
+        message: "catatan gagal ditambahkan",
+        
+    })
+
+    response.code(500)
+    return response
+}
+
+const getAllNotesHandler = ()=> ({
+    status: "success",
+    data: {
+        notes
+    }
+})
+
+const getNoteByIdHandler = (request, h) => {
+    const {id} = request.params
+
+    const note = notes.filter((n) => n.id === id)[0]
+
+    if(note != undefined){
+        return {
+            status: "success",
+            data : {
+                note 
+            }
+        }
+    }
+
+    const response = h.response({
+        status : "fail",
+        message: "catatan tidak ditemukan",
+    })
+
+    response.code(404)
+    return response
+    
+}
+
+const editNoteByIdHandler = (request, h) => {
+    const { id } = request.params 
+
+    const {title, tags, body } = request.payload
+
+    const updatedAt = new Date().toISOString()
+
+    const index = notes.findIndex((note)=> note.id === id)
+
+    if(index !== -1){
+        notes[index] = {
+            ...notes[index],
+            title,
+            tags,
+            body,
+            updatedAt
+
+        }
+        const response = h.response({
+            status: "success",
+            message: "catatan berhasil diperbaharui",
+    
+        })
+        response.code(200)
+        return response
+    }
+
+
+ 
+
+    const response = h.response({
+        status: "failed",
+        message: "catatan gagal diperbaharui"
+
+    })
+
+    response.code(404)
+    return response
+}
+
+const deleteNoteByIdHandler = (request, h) => {
+    const { id } = request.params
+
+    const index = notes.findIndex((note)=>note.id === id)
+
+    if( index !== -1 ){
+        notes.splice(index,1)
+        const response = h.response({
+            status: "success",
+            message: "data berhasil di delete !"            
+        })
+
+        response.code(200)
+        return response
+    }
+
+    const response = h.response({
+        status: "fail",
+        message: "data tidak berhasil di delete !"
+    })
+
+    response.code(404)
+    return response
+}
+
+module.exports = {addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler}
